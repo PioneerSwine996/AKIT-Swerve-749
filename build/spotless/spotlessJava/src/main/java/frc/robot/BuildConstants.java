d955ca041d0d723f01f128fe59b8b35d60a4c398
PioneerSwine996 <129558257+PioneerSwine996@users.noreply.github.com>
2025-04-02 10:20:36 -0700

    Still Still Importing

--- build/spotless/spotlessJava/src/main/java/frc/robot/BuildConstants.java
+++ build/spotless/spotlessJava/src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "AKIT-Swerve-749";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 1;
-  public static final String GIT_SHA = "8238a144965ea31589e7bb760749fcb0050f0213";
-  public static final String GIT_DATE = "2025-04-02 13:14:36 EDT";
+  public static final int GIT_REVISION = 3;
+  public static final String GIT_SHA = "7486de5d552ce5b82cc0a6a4fd054dd3ce11154f";
+  public static final String GIT_DATE = "2025-04-02 13:18:03 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-04-02 13:15:41 EDT";
-  public static final long BUILD_UNIX_TIME = 1743614141781L;
+  public static final String BUILD_DATE = "2025-04-02 13:18:56 EDT";
+  public static final long BUILD_UNIX_TIME = 1743614336994L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
